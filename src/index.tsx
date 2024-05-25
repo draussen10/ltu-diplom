cf9b8f5c115b68683442dc8b7511cf2ae9b1b7bf
draussen10 <a.a.boev@bk.ru>
2024-05-25 17:55:06 +0300

    Добавлен index для components и stores

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import { stores, StoreContext } from './stores/stores';
+import { index, StoreContext } from './stores';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <StoreContext.Provider value={stores}>
+        <StoreContext.Provider value={index}>
             <App />
         </StoreContext.Provider>
     </React.StrictMode>
